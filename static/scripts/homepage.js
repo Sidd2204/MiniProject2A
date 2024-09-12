ec2019cd7fe5d456456585788190d97315e9132b
Sidd2204 <224snp@gmail.com>
2024-09-12 14:11:28 +0530

    logout

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -1,1 +1,12 @@
+function logout() {
+  let action = confirm("Are You Sure?");
+  if (action) {
+    window.location.href = "http://127.0.0.1:5000/";
+  }
+}
 
+let submenu = document.getElementById("submenu");
+
+function togglemenu() {
+  submenu.classList.toggle("open-menu");
+}
